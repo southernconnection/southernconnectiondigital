e829d7c0a3b7babbd98116b60735bf90825b7d45
ymastin3 <89491660+ymastin3@users.noreply.github.com>
2024-11-27 18:14:15 -0500

    back to hybrid

--- astro.config.ts
+++ astro.config.ts
@@ -24,7 +24,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
 
   integrations: [
     tailwind({
